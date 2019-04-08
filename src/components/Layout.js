a5f9a396a86966a993b0b72ce519bfe395a13155
ganxunzou <ganxunzou@163.com>
2019-04-08 15:37:58 +0800

    basic

---
+++ src/components/Layout.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class Layout extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      title: 'Welcome to React SSR!',
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>{this.state.title}</h1>
+      </div>
+    );
+  }
+}
+
+export default Layout;
